27fbb031195d353dc6bfb43a63d9331132bb6304
Hammad124628 <hammad.ishtiaq1012@gmail.com>
2025-07-09 23:53:54 +0500

    api
    add api in the weather form

--- script.js
+++ script.js
@@ -5,38 +5,39 @@ function loginform() {
     const password = passwordref.value;
     console.log(email);
     console.log(password);
-   if (email === "hammad@gmail.com" && password === "1234") {
+    if (email === "hammad@gmail.com" && password === "1234") {
         // alert("Login successful");
-        const messageref= document.getElementById("message");
+        const messageref = document.getElementById("message");
         messageref.innerHTML = "you are signed in"
-    }else {
-        const messageref= document.getElementById("message");
+    } else {
+        const messageref = document.getElementById("message");
         messageref.innerHTML = "put your correct email or password"
 
     }
 }
 
-function weatherinfo(){
-  const cityref = document.getElementById("city");
-  const city = cityref.value;
-  console.log(city);
-
-  const Weatherref = document.getElementById("weathercondition");
-Weatherref.innerHTML="Good Condition";
+async function weatherinfo() {
+    const cityref = document.getElementById("city");
+    const city = cityref.value;
+    // console.log(city);
+    const weatherres = await fetch(`https://p2pclouds.up.railway.app/v1/learn/weather?city=${city}`);
+    const weather = await weatherres.json();
+    const Weatherref = document.getElementById("weathercondition");
+    const currenttem = weather.current.temp_c;
+    Weatherref.innerHTML=`Today Temperature of ${city} is ${currenttem}`;
 }
 
-
-function contactform(){
-    const nameref=document.getElementById("name");
+function contactform() {
+    const nameref = document.getElementById("name");
     const name = nameref.value;
     console.log(name);
-    const addressref=document.getElementById("Address");
+    const addressref = document.getElementById("Address");
     const Address = addressref.value;
     console.log(Address);
-    const numberref=document.getElementById("number");
-    const number = Number (numberref.value);
+    const numberref = document.getElementById("number");
+    const number = Number(numberref.value);
     console.log(number);
-     const emailref = document.getElementById("emails");
+    const emailref = document.getElementById("emails");
     const emails = emailref.value;
     console.log(emails);
     const passwordref = document.getElementById("passwords");
@@ -45,3 +46,5 @@ function contactform(){
     const contactmessageref = document.getElementById("contactmessage");
     contactmessageref.innerHTML = "Contact Form submitted successfully";
 }
+
+
